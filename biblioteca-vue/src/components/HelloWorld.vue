c91f4fdc339e08af0a2865eb2e3a4f331fcad280
RyanVictorDev <vryan8294@gmail.com>
2024-05-02 21:00:27 -0300

    Criando carrossel para a home page

--- biblioteca-vue/src/components/HelloWorld.vue
+++ biblioteca-vue/src/components/HelloWorld.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <h1>Olá</h1>
+    <v-carousel cycle hide-delimiter-background show-arrows-on-hover>
+    <v-carousel-item v-for="(slide, i) in slides" :key="i">
+      <v-sheet class="fill-height" :color="colors[i]" height="100%">
+        <v-row class="d-flex my-auto" justify="center">
+          <div fixed centered>
+            {{ slide }}
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>       
+  </v-carousel>
   </div>
 </template>
 
@@ -13,8 +23,22 @@
     },
 
     data:() => ({
-      // 
-    }),
+        colors: [
+          'error',
+          'warning',
+          'pink darken-2',
+          'red lighten-1',
+          'deep-purple accent-4',
+        ],
+        slides: [
+          'First',
+          'Second',
+          'Third',
+          'Fourth',
+          'Fifth',
+        ],
+      }
+    ),
   }
 </script>
 
